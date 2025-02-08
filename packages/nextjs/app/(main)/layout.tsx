c259276473244f2ead045e34142f429a56132170
alex <>
2025-02-08 21:45:02 +0900

    update: update the title and description

--- packages/nextjs/app/(main)/layout.tsx
+++ packages/nextjs/app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
+export const metadata = getMetadata({ title: "DexFlow", description: "The next generation decentralized exchange platform." });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   
